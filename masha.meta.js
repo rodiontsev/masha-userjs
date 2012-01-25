d30d17242e6cb473bd53234aff2bc1b8f730ab6d
Dmitry Rodiontsev <dmitry.rodiontsev@gmail.com>
2012-01-25 07:35:31 -0500

    Add meta.js for userscript

---
+++ masha.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name           Masha
+// @version        1.0.1
+// @namespace      http://twitter.com/dimarad
+// @description    Mark & Share
+// @author         Dmitry Rodiontsev
+// @include        http://*
+// @include        https://*
+// @resource masha masha.js
+// @resource style masha.css
+// ==/UserScript==
